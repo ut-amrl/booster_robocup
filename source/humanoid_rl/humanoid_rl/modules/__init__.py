cfe0ea678eda0b830bf2da959f269089c131e545
Mason Russell <mason.t.russell2.civ@army.mil>
2025-09-05 16:57:22 -0500

    adding student_teacher

--- source/humanoid_rl/humanoid_rl/modules/__init__.py
+++ source/humanoid_rl/humanoid_rl/modules/__init__.py
@@ -6,5 +6,7 @@
 from .actor_critic import ActorCritic
 from .actor_critic_recurrent import ActorCriticRecurrent
 from .normalizer import EmpiricalNormalization
+from .student_teacher import StudentTeacher
+from .student_teacher_recurrent import StudentTeacherRecurrent
 
-__all__ = ["ActorCritic", "ActorCriticRecurrent", "EmpiricalNormalization"]
+__all__ = ["ActorCritic", "ActorCriticRecurrent", "EmpiricalNormalization", "StudentTeacher", "StudentTeacherRecurrent"]
